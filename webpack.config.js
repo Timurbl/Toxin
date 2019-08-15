a70fc954a2b31989e486e67549530a4e1e4f554f
Timur <timurbl@mail.ru>
2019-08-15 09:52:16 +0300

    Подготовка webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
+
+const PATHS = {
+  source: path.join(__dirname, 'source'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = merge([
+  {
+    entry: PATHS.source + "/pages/index/index.js",
+    output: {
+      filename: "[name].js",
+      path: PATHS.build
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: PATHS.source + "/pages/index/index.pug"
+      })
+    ]
+  },
+  pug()
+]);
+
+module.exports = function (env) {
+  if (env === "production") {
+    return common
+  }
+  if (env === "development") {
+    return merge([
+      common,
+      devserver(),
+      sass()
+    ])
+  }
+};
